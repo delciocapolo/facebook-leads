902cf5a923310293a14ddae7de4ae336e2e0da18
delciocapolo <delciocapolo096@gmail.com>
2025-08-02 15:24:26 +0100

    feat: fixed command to start app

--- docker/app/entrypoint.sh
+++ docker/app/entrypoint.sh
@@ -4,11 +4,11 @@ echo "-> Current NODE_ENV is [$NODE_ENV]"
 
 if [ "$NODE_ENV" = "production" ]; then
   echo "-> Running application in production mode"
-  npm run start:prod
+  npm run start
 elif [ "$NODE_ENV" = "development" ]; then
   echo "-> Running application in development mode"
   npm run start:dev
 else
   echo "-> NODE_ENV not set or unknown. Falling back to default."
-  npm run start:prod
+  npm run start
 fi
